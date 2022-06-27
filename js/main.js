13a69f6769b8341ac9fefb3d914d9d924ac85936
vasara1987 <you@example.com>
2022-06-27 13:26:02 +0200

    formatted all files

--- js/main.js
+++ js/main.js
@@ -8,15 +8,15 @@ const displayTodoItem = ({
   title,
   id
 }) => {
-  const todoItemField = document.createElement('div'); 
+  const todoItemField = document.createElement('div');
   todoItemField.className = '.js-item-container item-container d-flex flex-nowrap justify-content-center mt-2';
 
   const todoItem = document.createElement('div');
   todoItem.className = '.js-item item border border-white border-2 rounded my-auto py-2 d-flex';
 
-  const checkbox = document.createElement('div');  
-  checkbox.className = '.js-checkbox checkbox mx-2';  
-  if (completed) checkbox.classList.add('checked'); 
+  const checkbox = document.createElement('div');
+  checkbox.className = '.js-checkbox checkbox mx-2';
+  if (completed) checkbox.classList.add('checked');
   checkbox.addEventListener('click', async () => {
     await ApiService.updateTodo({
       id,
@@ -26,13 +26,13 @@ const displayTodoItem = ({
   });
 
   const todoItemText = document.createElement('div');
-  todoItemText.className = '.js-item-text item-text'; 
-  todoItemText.innerText = title; 
+  todoItemText.className = '.js-item-text item-text';
+  todoItemText.innerText = title;
 
-  const buttonsDeleteAndEdit = document.createElement('div'); 
-  buttonsDeleteAndEdit.className = '.js-buttons buttons d-inline-flex'; 
+  const buttonsDeleteAndEdit = document.createElement('div');
+  buttonsDeleteAndEdit.className = '.js-buttons buttons d-inline-flex';
 
-  const btnDelete = document.createElement('button'); 
+  const btnDelete = document.createElement('button');
   btnDelete.className = '.btn-delete btn btn-primary mx-2';
   btnDelete.innerHTML = '<img class="button-img" src="assets/delete-button.png"/>';
   btnDelete.addEventListener('click', async () => {
@@ -40,14 +40,8 @@ const displayTodoItem = ({
     todoItem.remove();
   });
 
-  // const btnEdit = document.createElement('button');
-  // btnEdit.className = '.btn-edit btn btn-primary ms-2';
-  // btnEdit.innerHTML = '<img class ="button-image" src="assets/edit-pen.png"/>';
-  // btnEdit.addEventListener('click', console.log("Paspausta edit"));
-
-
   todoItemField.append(
-    todoItem,  
+    todoItem,
   );
 
   buttonsDeleteAndEdit.append(
@@ -75,61 +69,26 @@ const formAddTodo = new FormComponent(
 const todos = await ApiService.fetchTodos();
 todos.forEach(displayTodoItem);
 
-
-
-
 const numberOfInputs = todoList.children.length;
 console.log(numberOfInputs);
 
-// if(numberOfInputs > 0){
-//   const progressElement = document.createElement('div');
-
-// }
-const progressItem = document.querySelector('.js-progress-item');
-console.log(progressItem); 
+const progressItem = document.querySelector('.js-progress-field');
+console.log(progressItem);
 
 const arrayDiv = new Array();
 console.log(arrayDiv);
 for (let i = 0; i < numberOfInputs; i++) {
-    arrayDiv[i] = document.createElement('div');
-    progressItem.appendChild(arrayDiv[i]);
-      
+  arrayDiv[i] = document.createElement('div');
+  progressItem.appendChild(arrayDiv[i]);
 };
-
-// const item = document.querySelector('.js-progress-item > div');
-// item.classList.add("completed");
-// console.log(item);
 
 const numberOfCheckedItems = document.querySelectorAll(".checked").length;
 console.log(numberOfCheckedItems);
 
-
-// for (let i = 0; i < numberOfCheckedItems; i++) {
-//   const item = document.querySelector('.js-progress-item > div');
-//   console.log(item);
-//   item.classList.add("completed");
-      
-// };
-
-const items = document.querySelectorAll('.js-progress-item > div');
-console.log(items);
-const divsArr = Array.from(items);
-console.log(divsArr);
+const allInputsInProgressField = document.querySelectorAll('.js-progress-field > div');
+console.log(allInputsInProgressField);
+const allInputsInProgressFieldArr = Array.from(allInputsInProgressField);
+console.log(allInputsInProgressFieldArr);
 for (let i = 0; i < numberOfCheckedItems; i++) {
-  divsArr[i].classList.add("completed");
-
-;  
-
-
-
-      
+  allInputsInProgressFieldArr[i].classList.add("completed");
 };
-
-
-
-
- 
-
-
-
-
